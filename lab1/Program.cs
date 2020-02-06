d5c3e0e26adef156d447a9f73bbb5d7ddb28a4fa
eugene.kiselev <eugene.kiselev@financesoft.kg>
2020-02-06 11:36:15 +0600

    выполнил первую лабораторную работу

---
+++ lab1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var root in Tests.FindRootsOfSquareEquation(6,15,0))
+            {
+                Console.WriteLine(root);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab1/Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace lab1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase(1,-2,-3,ExpectedResult = new double[]{ 3,-1})]
+        [TestCase(-1,-2,15,ExpectedResult = new double[]{ -5,3})]
+        [TestCase(6,15,0,ExpectedResult = new double[]{ 0,-2.5})]
+        [TestCase(1,12,36,ExpectedResult = new double[]{ -6})]
+        [TestCase(8,0,0,ExpectedResult = new double[]{ 0 })]
+        [TestCase(0, 0, 0, ExpectedResult = null)]//нет корней, т.к. уравнение не квадратное
+        [TestCase(0, 8, 8, ExpectedResult = null)]//нет корней, т.к. уравнение не квадратное
+        [TestCase(5,0,30,ExpectedResult = null)]// нет корней, т.к. ответ в области комплексных чисел 
+        [TestCase(5,3,7,ExpectedResult = null)]// нет корней, т.к. ответ в области комплексных чисел
+        public double[] TestEquation(double a, double b, double c)
+        {
+            return FindRootsOfSquareEquation(a, b, c);
+        }
+
+        public static double[] FindRootsOfSquareEquation(double a, double b, double c)
+        {
+            double[] answer = null;
+
+            if (a != 0)
+            {
+                var discriminant = b * b - 4.0 * a * c;
+
+                if (discriminant == 0)
+                {
+                    answer = new[] { (-b / 2.0 * a) };
+                }
+                else if (discriminant > 0)
+                {
+                    answer = new[]
+                    {
+                        (-b + Math.Sqrt(discriminant))/(2.0*a),
+                        (-b - Math.Sqrt(discriminant))/(2.0*a)
+                    };
+                }
+            }
+
+            return answer;
+        }
+    }
+}
