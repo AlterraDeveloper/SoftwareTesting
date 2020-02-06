206112d63df1dd9657d5c32d25fe066e2e90b61d
eugene.kiselev <eugene.kiselev@financesoft.kg>
2020-02-06 15:12:58 +0600

    выполнил третью лабораторную работу

---
+++ lab1/lab3/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace SoftwareTesting.lab3
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        //[TestCase(1,-2,-3)]
+        //[TestCase(8, 0, 0)]
+        [TestCase(6, 15, 0)]
+        public void TestEquation(double a, double b, double c)
+        {
+            var result = lab1.Tests.FindRootsOfSquareEquation(a, b, c);
+            //Assert.AreEqual(new double[]{3,-1},result );
+            //Assert.AreEqual(new double[]{0},result );
+            CollectionAssert.AreEqual(new[]{0,-2.5},result);
+        }
+    }
+}
